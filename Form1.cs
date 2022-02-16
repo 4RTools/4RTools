0e2a839739010429cf1d2e849ef66672a77e9376
Marcus Cartágenes <mvcartagenes@gmail.com>
2022-02-16 10:45:24 -0300

    First Blood

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4RTools
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Utils/ProcessMemoryReader.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+
+namespace _4RTools.Utils
+{
+    internal class ProcessMemoryReader
+    {
+        private class MemoryApi
+        {
+            [Flags]
+            public enum AllocType
+            {
+                Commit = 0x1000,
+                Reserve = 0x2000,
+                Decommit = 0x4000
+            }
+
+            [Flags]
+            public enum Protect
+            {
+                Execute = 0x10,
+                ExecuteRead = 0x20,
+                ExecuteReadWrite = 0x40,
+            }
+            [Flags]
+            public enum FreeType
+            {
+                Decommit = 0x4000,
+                Release = 0x8000,
+            }
+
+            [Flags]
+            public enum ProcessAccessType
+            {
+                PROCESS_TERMINATE = 1,
+                PROCESS_CREATE_THREAD = 2,
+                PROCESS_SET_SESSIONID = 4,
+                PROCESS_VM_OPERATION = 8,
+                PROCESS_VM_READ = 16,
+                PROCESS_VM_WRITE = 32,
+                PROCESS_DUP_HANDLE = 64,
+                PROCESS_CREATE_PROCESS = 128,
+                PROCESS_SET_QUOTA = 256,
+                PROCESS_SET_INFORMATION = 512,
+                PROCESS_QUERY_INFORMATION = 1024
+            }
+
+            [DllImport("kernel32.dll")]
+            public static extern IntPtr OpenProcess(uint dwDesiredAccess, int bInheritHandle, uint dwProcessId);
+            [DllImport("kernel32.dll")]
+            public static extern int CloseHandle(IntPtr hObject);
+            [DllImport("kernel32.dll")]
+            public static extern int ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [In][Out] byte[] buffer, uint size, out IntPtr lpNumberOfBytesRead);
+            [DllImport("kernel32.dll")]
+            public static extern int WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [In][Out] byte[] buffer, uint size, out IntPtr lpNumberOfBytesWritten);
+
+            [DllImport("kernel32.dll")]
+            public static extern Int32 VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, int dwSize, AllocType flAllocationType, Protect flProtect);
+            [DllImport("kernel32.dll")]
+            public static extern bool VirtualFreeEx(IntPtr hProcess, IntPtr lpAddress, int dwSize, FreeType dwFreeType);
+        }
+
+        private Process m_ReadProcess = null;
+        private IntPtr m_hProcess = IntPtr.Zero;
+        public Process ReadProcess
+        {
+            get
+            {
+                return this.m_ReadProcess;
+            }
+            set
+            {
+                this.m_ReadProcess = value;
+            }
+        }
+        public void OpenProcess()
+        {
+            ProcessMemoryReader.MemoryApi.ProcessAccessType dwDesiredAccess = ProcessMemoryReader.MemoryApi.ProcessAccessType.PROCESS_VM_OPERATION | ProcessMemoryReader.MemoryApi.ProcessAccessType.PROCESS_VM_READ | ProcessMemoryReader.MemoryApi.ProcessAccessType.PROCESS_VM_WRITE;
+            this.m_hProcess = ProcessMemoryReader.MemoryApi.OpenProcess((uint)dwDesiredAccess, 1, (uint)this.m_ReadProcess.Id);
+        }
+        public void CloseHandle()
+        {
+            int num = ProcessMemoryReader.MemoryApi.CloseHandle(this.m_hProcess);
+            if (num == 0)
+            {
+                throw new Exception("CloseHandle failed");
+            }
+        }
+        public byte[] ReadProcessMemory(IntPtr MemoryAddress, uint bytesToRead, out int bytesRead)
+        {
+            byte[] array = new byte[bytesToRead];
+            IntPtr intPtr;
+            ProcessMemoryReader.MemoryApi.ReadProcessMemory(this.m_hProcess, MemoryAddress, array, bytesToRead, out intPtr);
+            bytesRead = intPtr.ToInt32();
+            return array;
+        }
+        public void WriteProcessMemory(IntPtr MemoryAddress, byte[] bytesToWrite, out int bytesWritten)
+        {
+            IntPtr intPtr;
+            ProcessMemoryReader.MemoryApi.WriteProcessMemory(this.m_hProcess, MemoryAddress, bytesToWrite, (uint)bytesToWrite.Length, out intPtr);
+            bytesWritten = intPtr.ToInt32();
+        }
+
+
+        public void Alloc(out int Addr, int Size)
+        {
+            Addr = ProcessMemoryReader.MemoryApi.VirtualAllocEx(m_hProcess, IntPtr.Zero, Size, ProcessMemoryReader.MemoryApi.AllocType.Commit, ProcessMemoryReader.MemoryApi.Protect.ExecuteReadWrite);
+        }
+        public bool Dealloc(int Addr)
+        {
+            return ProcessMemoryReader.MemoryApi.VirtualFreeEx(m_hProcess, (IntPtr)Addr, 0, ProcessMemoryReader.MemoryApi.FreeType.Release);
+        }
+    }
+}
