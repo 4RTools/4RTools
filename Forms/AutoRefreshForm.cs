6516bcac142da719be16b81b402715dfb437e18c
Daniel Afonso <daniel.acfilho@gmail.com>
2022-02-25 02:59:30 -0300

    feat(Forms/AutoRefreshForm): implement the auto refresh from with key and delay

---
+++ Forms/AutoRefreshForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Forms;
+using _4RTools.Utils;
+using _4RTools.Model;
+using System.Windows.Input;
+
+namespace _4RTools.Forms
+{
+    public partial class AutoRefreshForm : Form, IObserver
+    {
+        private AutoRefreshBuff autoRefreshBuff = new AutoRefreshBuff();
+
+        public AutoRefreshForm(Subject subject)
+        {
+            InitializeComponent();
+            subject.Attach(this);
+
+            // Default values
+            this.cbRefreshKey.DataSource = new BindingSource(KeyMap.getDict(), null);
+            this.cbRefreshKey.SelectedValue = Key.None;
+        }
+
+        public void Update(ISubject subject)
+        {
+            this.autoRefreshBuff.Start();
+        }
+
+        private void autoRefreshKeyIndexChanged(object sender, EventArgs e)
+        {
+            this.autoRefreshBuff.refreshKey = (Key)this.cbRefreshKey.SelectedValue;
+        }
+
+        private void txtAutoRefreshDelayTextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                this.autoRefreshBuff.refreshDelay = Int16.Parse(this.txtAutoRefreshDelay.Text);
+            }
+            catch (Exception) { }
+        }
+    }
+}
