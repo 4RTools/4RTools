d29f4dbf1f74559384866ea2373698330d2da978
Daniel Afonso <daniel.acfilho@gmail.com>
2022-02-26 16:20:41 -0300

    feat(Model/AHK): improve skill interval

--- Model/AHK.cs
+++ Model/AHK.cs
@@ -37,7 +37,7 @@ namespace _4RTools.Model
                                     Keys thisk = (Keys)Enum.Parse(typeof(Keys), key.ToString());
                                     Interop.PostMessage(roClient.process.MainWindowHandle, Constants.WM_KEYDOWN_MSG_ID, thisk, 0);
                                     Interop.PostMessage(roClient.process.MainWindowHandle, Constants.WM_LBUTTONDOWN, 0, 0);
-                                    Thread.Sleep(10);
+                                    Thread.Sleep(1);
                                     Interop.PostMessage(roClient.process.MainWindowHandle, Constants.WM_LBUTTONUP, 0, 0);
                                     Thread.Sleep(ahkDelay);
                                 }
