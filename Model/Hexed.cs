5833d479862b08ccd601fb1f0e4efa111220a62c
Marcus Cartágenes <mvcartagenes@gmail.com>
2022-03-07 22:20:19 -0300

    chore: Adding PowkRO, Maniacos and RememberRO client addresses

--- Model/Hexed.cs
+++ Model/Hexed.cs
@@ -168,6 +168,9 @@ namespace _4RTools.Model
             List<Client> result = new List<Client>();
             result.Add(new Client(2019, 0x00E8E434, 0x00E90C00)); //Clients2019 (Tales, etc..)
             result.Add(new Client(2018, 0x0101A700, 0x0101CEB0)); //Clients2018 (Portal Kafra, etc...)
+            result.Add(new Client(2017, 0x00D1CA6C, 0x00D1D288)); //Clients2017 (PowkRO, etc...)
+            result.Add(new Client(2017, 0x00BC67E8, 0x00E4D768)); //Clients2017 (Maniacos, etc...)
+            result.Add(new Client(2016, 0x0083E1B4, 0x00E90C00)); //Clients2016 (Remember RO, etc...)
             return result;
         }
 
