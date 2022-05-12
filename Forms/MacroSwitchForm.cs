3375f738abb7ba77b5a249891b993796b3964ee8
Daniel Filho <daniel.acfilho@gmail.com>
2022-05-12 18:08:56 -0300

    feat(Forms): macro switch UI (#84)
    * feat(Forms): create macro switch form UI
    
    * chore(Resources): add new resources icon
    
    * chore(4RTools.csproj): add resources arrow up adn arrowdown
    
    * feat(Container): show macro switch tab

---
+++ Forms/MacroSwitchForm.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+using _4RTools.Utils;
+
+namespace _4RTools.Forms
+{
+    public partial class MacroSwitchForm : Form
+    {
+        public MacroSwitchForm(Subject subject)
+        {
+            InitializeComponent();
+        }
+    }
+}
