885d9f46ab63c889906db8e8a946fd0e59b17be8
Daniel Afonso <daniel.acfilho@gmail.com>
2022-05-21 15:08:19 -0300

    refactor(Utils/AppConfig): renam to app config and set all attributes static

---
+++ Utils/AppConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4RTools.Utils
+{
+    internal class AppConfig
+    {
+        public static string Name = "4RTools";
+        public static string ProfileFolder = "Profile\\";
+        public static string Website = "https://www.4rtools.com.br";
+        public static string GithubLink = "https://github.com/4RTools/4Rtools";
+        public static string DiscordLink = "https://discord.gg/AtZ2fJVtBz";
+        public static string Version = "1.7.0";
+    }
+}
