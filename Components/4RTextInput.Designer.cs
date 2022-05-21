6bd3b1ffa9fd7fb69f02b3e5ae3bf0a585445d26
Marcus Cartágenes <mvcartagenes@gmail.com>
2022-05-21 11:31:55 -0300

    Adding Custom Text Input

---
+++ Components/4RTextInput.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace _4RTools.Components
+{
+    partial class _4RTextInput
+    {
+        /// <summary> 
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Designer de Componentes
+
+        /// <summary> 
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F);
+            this.textBox1.Location = new System.Drawing.Point(7, 7);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(236, 11);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            this.textBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseDown);
+            this.textBox1.MouseEnter += new System.EventHandler(this.textBox1_MouseEnter);
+            // 
+            // _4RTextInput
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.DimGray;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "_4RTextInput";
+            this.Padding = new System.Windows.Forms.Padding(7);
+            this.Size = new System.Drawing.Size(250, 30);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ Components/4RTextInput.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace _4RTools.Components
+{
+    [DefaultEvent("_TextChanged")]
+    public partial class _4RTextInput : UserControl
+    {
+
+        private Color borderColor = Color.MediumSlateBlue;
+        private int borderSize = 2;
+        private bool underlinedStyle = false;
+        private Color borderFocusColor = Color.HotPink;
+
+
+        public _4RTextInput()
+        {
+            InitializeComponent();
+        }
+
+        //Events
+        public event EventHandler _TextChanged;
+
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics graph = e.Graphics;
+
+            //Draw Border;
+            using (Pen penBorder = new Pen(borderColor, borderSize))
+            {
+                penBorder.Alignment = System.Drawing.Drawing2D.PenAlignment.Inset;
+
+                if (!textBox1.Focused)
+                {
+                    if (underlinedStyle) //Line Style
+                        graph.DrawLine(penBorder, 0, this.Height - 1, this.Width, this.Height - 1);
+                    else //Normal Style
+                        graph.DrawRectangle(penBorder, 0, 0, this.Width - 0.5f, this.Height - 0.5f);
+                }
+                else
+                {
+                    penBorder.Color = borderFocusColor;
+                    if (underlinedStyle) //Line Style
+                        graph.DrawLine(penBorder, 0, this.Height - 1, this.Width, this.Height - 1);
+                    else //Normal Style
+                        graph.DrawRectangle(penBorder, 0, 0, this.Width - 0.5f, this.Height - 0.5f);
+                }
+
+               
+            }
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            if(this.DesignMode)
+                UpdateControlHeight();
+            
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            UpdateControlHeight();
+        }
+
+        private void UpdateControlHeight()
+        {
+            if (textBox1.Multiline == false)
+            {
+                int txtHeight = TextRenderer.MeasureText("Text", this.Font).Height + 1;
+                textBox1.Multiline = true;
+                textBox1.MinimumSize = new Size(0, txtHeight);
+                textBox1.Multiline = false;
+
+                this.Height = textBox1.Height + this.Padding.Top + this.Padding.Bottom;
+            }
+        }
+
+
+        [Category("4R Custom Components")]
+        public override Color BackColor
+        {
+            get => base.BackColor; 
+            set
+            {
+                base.BackColor = value;
+                textBox1.BackColor = value; 
+            }
+        }
+
+        [Category("4R Custom Components")]
+        public override Color ForeColor
+        {
+            get => base.ForeColor; 
+            set
+            {
+                base.ForeColor = value;
+                textBox1.ForeColor = value;
+            }
+        }
+
+        [Category("4R Custom Components")]
+        public override Font Font
+        {
+            get => base.Font; 
+            set
+            {
+                base.Font = value;
+                textBox1.Font = value;
+            }
+        }
+
+        [Category("4R Custom Components")]
+        public string Value
+        {
+            get => textBox1.Text; 
+            set
+            {
+                textBox1.Text = value;
+            }
+        }
+
+        [Category("4R Custom Components")]
+        public Color BorderColor { get => borderColor; set {
+                borderColor = value;
+                this.Invalidate();
+            } }
+        [Category("4R Custom Components")]
+        public int BorderSize { get => borderSize; set {
+                borderSize = value;
+                this.Invalidate();
+            } }
+        [Category("4R Custom Components")]
+        public bool UnderlinedStyle { get => underlinedStyle; set {
+                underlinedStyle = value;
+                this.Invalidate();
+            } }
+
+        [Category("4R Custom Components")]
+        public Color BorderFocusColor { get => borderFocusColor; set => borderFocusColor = value; }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if(_TextChanged != null)
+            {
+                _TextChanged.Invoke(sender, e);
+            }
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            this.Invalidate();
+            this.OnClick(e);
+        }
+
+        private void textBox1_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            this.OnMouseDown(e);
+        }
+
+        private void textBox1_MouseEnter(object sender, EventArgs e)
+        {
+            this.OnMouseEnter(e);
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void textBox1_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            try
+            {
+                Key thisk = (Key)Enum.Parse(typeof(Key), e.KeyCode.ToString());
+
+                switch (thisk)
+                {
+                    case Key.Escape:
+                    case Key.Back:
+                        textBox1.Text = Key.None.ToString();
+                        break;
+                    default:
+                        textBox1.Text = e.KeyCode.ToString();
+                        break;
+                }
+                textBox1.Enabled = false;
+                textBox1.Enabled = true;
+                this.Invalidate();
+                e.Handled = true;
+            }
+            catch { }
+        }
+
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+    }
+}
