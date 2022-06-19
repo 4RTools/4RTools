251f20f964222fde4f45c9011cfc4ba3cd73953f
Daniel Afonso <daniel.acfilho@gmail.com>
2022-06-19 01:48:47 -0300

    chore(Hexed): add AllureWar server

--- Model/Hexed.cs
+++ Model/Hexed.cs
@@ -195,6 +195,7 @@ namespace _4RTools.Model
             clients.Add(new Client("BlueRO",0x011D1A04, 0x011D43E8));
             clients.Add(new Client("StreetRO 2.0",0x010DCE10, 0x010D5DA8));
             clients.Add(new Client("Gladius", 0x010DCE10, 0x010DF5D8));
+            clients.Add(new Client("AllureWar", 0x01144144, 0x01146A18));
 
             return clients;
         }
