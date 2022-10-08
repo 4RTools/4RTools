ac33026a27ea6e1523ec397da305763cc058a98b
Daniel Filho <daniel.acfilho@gmail.com>
2022-10-08 13:18:08 -0300

    fix(Autobuff): fix autobuff list and increase the delay (#167)
    * fix(Autobuff): fix autobuff list and increase the delay
    
    * refactor(autobuff): remove -1 from the MAX_BUFF_LIST

--- Model/Autobuff.cs
+++ Model/Autobuff.cs
@@ -35,7 +35,7 @@ namespace _4RTools.Model
 
                 bool foundQuag = false;
                 Dictionary<EffectStatusIDs, Key> bmClone = new Dictionary<EffectStatusIDs, Key>(this.buffMapping);
-                for (int i = 1; i < Constants.MAX_BUFF_LIST_INDEX_SIZE - 1; i++)
+                for (int i = 0; i < Constants.MAX_BUFF_LIST_INDEX_SIZE; i++)
                 {
                     uint currentStatus = c.CurrentBuffStatusCode(i);
                     EffectStatusIDs status = (EffectStatusIDs)currentStatus;
@@ -61,7 +61,7 @@ namespace _4RTools.Model
                     }
                 }
 
-                Thread.Sleep(100);
+                Thread.Sleep(300);
                 return 0;
 
             });
