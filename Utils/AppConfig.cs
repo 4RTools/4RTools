ec31538c7baff7c3ec8f00c205cf8f5e6ab5bfef
Daniel Afonso <daniel.acfilho@gmail.com>
2023-10-22 13:18:11 -0300

    chore(AppConfig): bump version v2.6.0

--- Utils/AppConfig.cs
+++ Utils/AppConfig.cs
@@ -14,6 +14,6 @@ namespace _4RTools.Utils
         public static string _4RAdvertiserUrl = "https://storage.googleapis.com/4rtools/advertisers.json";
         public static string _4RLatestVersionURL = "https://api.github.com/repos/4RTools/4RTools/releases/latest";
         public static string _4RApiHost = "https://api.4rtools.com.br/api";
-        public static string Version = "v2.5.1";
+        public static string Version = "v2.6.0";
     }
 }
