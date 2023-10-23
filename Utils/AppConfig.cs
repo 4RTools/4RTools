376cefd07976432f3ab5c2e1dd118d2f2064e887
heckercat <53240858+holic-cat@users.noreply.github.com>
2023-10-23 16:21:16 +0800

    Update AppConfig.cs

--- Utils/AppConfig.cs
+++ Utils/AppConfig.cs
@@ -11,7 +11,7 @@ namespace _4RTools.Utils
         public static string GithubLink = "https://github.com/4RTools/4Rtools";
         public static string DiscordLink = "https://discord.gg/AtZ2fJVtBz";
         public static string _4RClientsURL = "https://storage.googleapis.com/4rtools/supported_servers.json";
-        public static string _4RAdvertiserUrl = "https://storage.googleapis.com/4rtools/advertisers.json";
+        public static string _4RAdvertiserUrl = "https://patcher.elvesro.com/4rtools/advertisers.json";
         public static string _4RLatestVersionURL = "https://api.github.com/repos/4RTools/4RTools/releases/latest";
         public static string _4RApiHost = "https://api.4rtools.com.br/api";
         public static string Version = "v2.6.0";
