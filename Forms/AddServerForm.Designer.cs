542593e0f931549097c94ee03c256b01a3ea6b2a
Daniel Afonso <daniel.acfilho@gmail.com>
2024-10-19 18:50:06 -0300

    chore(AddServerForm): does not alow minimize the window

--- Forms/AddServerForm.Designer.cs
+++ Forms/AddServerForm.Designer.cs
@@ -290,6 +290,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "AddServerForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Add New Server";
