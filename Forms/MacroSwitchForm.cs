0129cfcfdba6b4164197878b61afdfd9bf9b336b
Marcus Cartágenes <mvcartagenes@gmail.com>
2025-03-10 21:47:57 -0300

    wip: macro with click

--- Forms/MacroSwitchForm.cs
+++ Forms/MacroSwitchForm.cs
@@ -5,12 +5,14 @@ using System.Collections.Generic;
 using _4RTools.Model;
 using _4RTools.Utils;
 using System.Text.RegularExpressions;
+using System.Diagnostics;
+using System.Xml.Linq;
 
 namespace _4RTools.Forms
 {
     public partial class MacroSwitchForm : Form, IObserver
     {
-        public static int TOTAL_MACRO_LANES = 3;
+        public static int TOTAL_MACRO_LANES = 4;
         public MacroSwitchForm(Subject subject)
         {
             subject.Attach(this);
@@ -58,17 +60,25 @@ namespace _4RTools.Forms
                         NumericUpDown delayInput = (NumericUpDown)d[0];
                         delayInput.Value = chainConfig.macroEntries[cbName].delay;
                     }
+
+                    Control[] c = group.Controls.Find($"{cbName}click", true); // Clicks
+                    if (d.Length > 0)
+                    {
+                        CheckBox checkInput = (CheckBox)c[0];
+                        checkInput.Checked = chainConfig.macroEntries[cbName].hasClick;
+                    }
+
+
                 }
             }
             catch { };
         }
 
-        private void onTextChange(object sender, EventArgs e)
+        private void onTextChange(object sender, EventArgs e, ChainConfig chainConfig)
         {                 
             TextBox textBox = (TextBox)sender;
             int chainID = Int16.Parse(textBox.Parent.Name.Split(new[] { "chainGroup" }, StringSplitOptions.None)[1]);
             GroupBox group = (GroupBox)this.Controls.Find("chainGroup" + chainID, true)[0];
-            ChainConfig chainConfig = ProfileSingleton.GetCurrent().MacroSwitch.chainConfigs.Find(config => config.id == chainID);
 
             Key key = (Key)Enum.Parse(typeof(Key), textBox.Text.ToString());
             NumericUpDown delayInput = (NumericUpDown)group.Controls.Find($"{textBox.Name}delay", true)[0];
@@ -80,15 +90,20 @@ namespace _4RTools.Forms
             ProfileSingleton.SetConfiguration(ProfileSingleton.GetCurrent().MacroSwitch);
         }
 
-        private void onDelayChange(object sender, EventArgs e)
+        private void onDelayChange(object sender, EventArgs e, ChainConfig chainConfig)
         {
             NumericUpDown delayInput = (NumericUpDown)sender;
-            int chainID = Int16.Parse(delayInput.Parent.Name.Split(new[] { "chainGroup" }, StringSplitOptions.None)[1]);
-            ChainConfig chainConfig = ProfileSingleton.GetCurrent().MacroSwitch.chainConfigs.Find(config => config.id == chainID);
-
             String cbName = delayInput.Name.Split(new[] { "delay" }, StringSplitOptions.None)[0];
             chainConfig.macroEntries[cbName].delay = decimal.ToInt16(delayInput.Value);
+            ProfileSingleton.SetConfiguration(ProfileSingleton.GetCurrent().MacroSwitch);
+        }
 
+        private void onCheckClickChange(object sender, EventArgs e, ChainConfig chainConfig)
+        {
+            CheckBox checkInput = (CheckBox)sender;
+            int chainID = Int16.Parse(checkInput.Parent.Name.Split(new[] { "chainGroup" }, StringSplitOptions.None)[1]);
+            String cbName = checkInput.Name.Split(new[] { "click" }, StringSplitOptions.None)[0];
+            chainConfig.macroEntries[cbName].hasClick = checkInput.Checked;
             ProfileSingleton.SetConfiguration(ProfileSingleton.GetCurrent().MacroSwitch);
         }
 
@@ -115,18 +130,32 @@ namespace _4RTools.Forms
                 GroupBox p = (GroupBox)this.Controls.Find("chainGroup" + id, true)[0];
                 foreach (Control control in p.Controls)
                 {
+                    ChainConfig chainConfig = ProfileSingleton.GetCurrent().MacroSwitch.chainConfigs.Find(config => config.id == id);
+
+                    if (chainConfig == null) {
+                        chainConfig = new ChainConfig(id, Key.None);
+                        ProfileSingleton.GetCurrent().MacroSwitch.chainConfigs.Add(chainConfig);
+                    }
+
                     if (control is TextBox)
                     {
                         TextBox textBox = (TextBox)control;
                         textBox.KeyDown += new System.Windows.Forms.KeyEventHandler(FormUtils.OnKeyDown);
                         textBox.KeyPress += new KeyPressEventHandler(FormUtils.OnKeyPress);
-                        textBox.TextChanged += new EventHandler(this.onTextChange);
+                        textBox.TextChanged += new EventHandler((object sender, EventArgs art) => this.onTextChange(sender, art, chainConfig));
                     }
 
                     if (control is NumericUpDown)
                     {
                         NumericUpDown delayInput = (NumericUpDown)control;
-                        delayInput.ValueChanged += new System.EventHandler(this.onDelayChange);
+                        delayInput.ValueChanged += new System.EventHandler((object sender, EventArgs args) => this.onDelayChange(sender, args, chainConfig));
+                    }
+
+
+                    if (control is CheckBox)
+                    {
+                        CheckBox checkInput = (CheckBox)control;
+                        checkInput.CheckedChanged += new System.EventHandler((object sender, EventArgs args) => this.onCheckClickChange(sender, args, chainConfig));
                     }
                 }
             }
